5086166a26bb0ae655ddfa0370aa8dcabda93150
paradro1d <gorbunov.sm@phystech.edu>
2022-07-21 04:34:32 +0300

    Added residual connections and multi-step loss

--- model.py
+++ model.py
@@ -1,32 +1,37 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, BatchNormalization, ReLU, Input
+from tensorflow.keras.layers import Dense, BatchNormalization, ReLU, Input, Add
+from normalizer import Normalizer
 
-def build_net(network_structure):
-	#Build network with given structure in list format:
-	#	[input, layer1, layer2, ..., output]
-	input_layer = Input(shape=(network_structure[0]))
-	model_layer = Dense(network_structure[1])(input_layer)
-	#model_layer = BatchNormalization()(model_layer)
-	model_layer = ReLU()(model_layer)
-	for layer in network_structure[2:-1]:
-		model_layer = Dense(layer)(model_layer)
-		#model_layer = BatchNormalization()(model_layer)
-		model_layer = ReLU()(model_layer)
-	model_layer = Dense(network_structure[-1])(model_layer)
+def residual_block(x: tf.Tensor, width: int) -> tf.Tensor:
+	first_layer = Dense(width, activation='relu')(x)
+	second_layer = Dense(width, activation='relu')(first_layer)
+	last_layer = Add()([x, second_layer])
+	return last_layer
+
+def build_net(network_structure: dict):
+	inp = network_structure['input']
+	width = network_structure['width']
+	n = network_structure['blocks_num']
+	out = network_structure['output']
+	input_layer = Input(shape=(network_structure['input']))
+	model_layer = Dense(width, activation='relu')(input_layer)
+	for _ in range(n):
+		model_layer = residual_block(model_layer, width)
+	model_layer = Dense(out)(model_layer)
 	return tf.keras.models.Model(inputs=input_layer, outputs=model_layer)
 
 
 class Gnn_basic(tf.keras.Model):
 	#Basic graph neural network model
 
-	def __init__(self, edge_encoder, node_encoder, edge_processor, node_processor, decoder, loss_function, normalizer):
+	def __init__(self, nn_params, loss_function, normalizer):
 		#Initialization of model with given network parameters
 		super(Gnn_basic, self).__init__()
-		self.edge_encoder = build_net(edge_encoder)
-		self.node_encoder = build_net(node_encoder)
-		self.edge_processor = build_net(edge_processor)
-		self.node_processor = build_net(node_processor)
-		self.decoder = build_net(decoder)
+		self.edge_encoder = build_net(nn_params['edge_encoder'])
+		self.node_encoder = build_net(nn_params['node_encoder'])
+		self.edge_processor = build_net(nn_params['edge_processor'])
+		self.node_processor = build_net(nn_params['node_processor'])
+		self.decoder = build_net(nn_params['decoder'])
 		self.loss_function = loss_function
 		self.normalizer = normalizer
 
@@ -81,13 +86,31 @@ class Gnn_basic(tf.keras.Model):
 		edges = batch[0]
 		edge_features = batch[1]
 		node_features = batch[2][0]
-		node_features_true = tf.cast(batch[2][1], tf.float32)
-		node_features_true = self.normalizer.normalize_nodes(node_features_true)
+		node_features_t1 = tf.cast(batch[2][1], tf.float32)
+		node_features_t1 = self.normalizer.normalize_nodes(node_features_true)
+		node_features_t2 = tf.cast(batch[2][2], tf.float32)
+		node_features_t2 = self.normalizer.normalize_nodes(node_features_true)
+		node_features_t3 = tf.cast(batch[2][3], tf.float32)
+		node_features_t3 = self.normalizer.normalize_nodes(node_features_true)
+		node_features_t4 = tf.cast(batch[2][4], tf.float32)
+		node_features_t4 = self.normalizer.normalize_nodes(node_features_true)
+
 
 		with tf.GradientTape() as tape:
+			loss = 0
 			node_features_predicted = self.call((edges, node_features, edge_features))
-			node_features_predicted = self.normalizer.normalize_nodes(node_features_predicted)
-			loss = self.loss_function(node_features_predicted, node_features_true)
+			node_features_predicted_norm = self.normalizer.normalize_nodes(node_features_predicted)
+			loss += self.loss_function(node_features_predicted_norm, node_features_t1)
+			node_features_predicted = self.call((edges, node_features_predicted, edge_features))
+			node_features_predicted_norm = self.normalizer.normalize_nodes(node_features_predicted)
+			loss += self.loss_function(node_features_predicted_norm, node_features_t2)
+			node_features_predicted = self.call((edges, node_features_predicted, edge_features))
+			node_features_predicted_norm = self.normalizer.normalize_nodes(node_features_predicted)
+			loss += self.loss_function(node_features_predicted_norm, node_features_t3)
+			node_features_predicted = self.call((edges, node_features_predicted, edge_features))
+			node_features_predicted_norm = self.normalizer.normalize_nodes(node_features_predicted)
+			loss += self.loss_function(node_features_predicted_norm, node_features_t4)
+
 
 		trainable_variables = (self.edge_encoder.trainable_variables
 			+ self.node_encoder.trainable_variables
@@ -96,4 +119,16 @@ class Gnn_basic(tf.keras.Model):
 			+ self.decoder.trainable_variables)
 		grads = tape.gradient(loss, trainable_variables)
 		self.optimizer.apply_gradients(zip(grads, trainable_variables))
+		return {'Loss':loss}
+
+	@tf.function()
+	def test_step(self, batch):
+		edges = batch[0]
+		edge_features = batch[1]
+		node_features = batch[2][0]
+		node_features_true = tf.cast(batch[2][1], tf.float32)
+		node_features_true = self.normalizer.normalize_nodes(node_features_true)
+		node_features_predicted = self.call((edges, node_features, edge_features))
+		node_features_predicted = self.normalizer.normalize_nodes(node_features_predicted)
+		loss = self.loss_function(node_features_predicted, node_features_true)
 		return {'Loss':loss}
